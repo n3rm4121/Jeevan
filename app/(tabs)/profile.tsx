50880d8ca178992e77e5d741542e81cabd77d778
n3rm4121 <nirmaldhakal2032@gmail.com>
2024-12-05 10:37:45 +0545

    add profile component with email and password input for user registration

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,15 +1,51 @@
-import { Button } from 'react-native';
+import { Button, View } from 'react-native';
 import { app } from '../../utils/firebaseConfig'
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { collection, getDocs } from "firebase/firestore"
+import { Input } from '~/components/ui/input';
+import React from 'react';
 
 
 export default function Profile() {
+    const auth = getAuth(app);
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
 
+    const onChangeText = (text: string) => {
+        setEmail(text);
+    };
+    const onChangePassword = (text: string) => {
+        setPassword(text);
+    }
 
     return (
-        <>
-
-        </>
+        <View>
+            <Input
+                placeholder='Enter your email'
+                value={email}
+                onChangeText={onChangeText}
+                aria-labelledby='inputLabel'
+                aria-errormessage='inputError'
+            />
+            <Input
+                placeholder='Enter your password'
+                value={password}
+                onChangeText={onChangePassword}
+                aria-labelledby='inputLabel'
+                aria-errormessage='inputError'
+            />
+            <Button title='Sign Up' onPress={() => createUserWithEmailAndPassword(auth, email, password)
+                .then((userCredential) => {
+                    // Signed in 
+                    const user = userCredential.user;
+                    console.log(user);
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    console.log(errorMessage);
+                })
+            } />
+        </View>
     )
 }
--- utils/firebaseConfig.ts
+++ utils/firebaseConfig.ts
@@ -22,3 +22,5 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 // For more information on how to access Firebase in your project,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
+
+const auth = getAuth(app);
